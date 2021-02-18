d9ee705131f8687025e1f5be6efe1e2fc6156529
Lulin <kistssy+dev@gmail.com>
2021-02-18 21:28:35 +0900

    Implement saving images

---
+++ save_images.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+import tqdm
+import utils
+
+# 디렉토리 설정
+root = 'D:/demo'
+save_dir = 'images'
+os.makedirs(save_dir, exist_ok=True)
+
+# 이미지 경로 가져오기
+paths_input_image = utils.get_image_path(root, 'input_image')
+paths_groundtruth = utils.get_image_path(root, 'groundtruth')
+paths_conventional = utils.get_image_path(root, 'conventional')
+paths_proposed = utils.get_image_path(root, 'proposed')
+assert len(paths_input_image) == len(paths_groundtruth) == len(paths_conventional) == len(paths_proposed)
+
+# matplotlib rcParams 설정
+utils.set_matplotlib_rcparams()
+
+# plot 제작 후 저장
+zip_iter = zip(paths_input_image, paths_groundtruth, paths_conventional, paths_proposed)
+for paths in tqdm.tqdm(zip_iter, total=len(paths_input_image)):
+    input_image = Image.open(paths[0])
+    groundtruth = Image.open(paths[1])
+    conventional = Image.open(paths[2])
+    proposed = Image.open(paths[3])
+    input_image = input_image.resize(groundtruth.size)
+    assert input_image.size == groundtruth.size == conventional.size == proposed.size
+
+    image_name = os.path.split(paths[0])[-1]
+    titles = ['Input image ({})'.format(image_name), 'Groundtruth', 'Conventional', 'Proposed']
+    images = [input_image, groundtruth, conventional, proposed]
+    utils.make_plot(titles, images, os.path.join(save_dir, image_name))
---
+++ utils.py
@@ -0,0 +1,34 @@
+import glob
+import os
+import matplotlib.pyplot as plt
+
+
+# 이미지 경로를 가져옴
+def get_image_path(root: str, sub_dir: str):
+    paths = glob.glob(os.path.join(root, sub_dir, '*'))
+    paths.sort()
+    return paths
+
+
+# plot을 제작
+def make_plot(titles: list, images: list, save_plot: str = None, show_plot=False):
+    fig, axs = plt.subplots(2, 2)
+    for ax in axs.reshape(4):
+        ax.set_title(titles.pop(0))
+        ax.imshow(images.pop(0))
+
+    if save_plot is not None:
+        plt.savefig(save_plot)
+    if show_plot:
+        plt.show()
+    plt.close()
+
+
+# matplotlib rcParams를 설정
+def set_matplotlib_rcparams():
+    plt.rcParams['figure.figsize'] = (18, 9.5)
+    plt.rcParams['figure.autolayout'] = True
+    plt.rcParams['xtick.bottom'] = False
+    plt.rcParams['xtick.labelbottom'] = False
+    plt.rcParams['ytick.left'] = False
+    plt.rcParams['ytick.labelleft'] = False
